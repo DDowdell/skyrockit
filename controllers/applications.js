ab5d615b85b1b8b9d353abc28e7914300b78a9fe
Dae Dowdell <dow_dell2010@yahoo.com>
2025-07-24 13:29:34 -0400

    add new functionality

--- controllers/applications.js
+++ controllers/applications.js
@@ -5,6 +5,7 @@ const User = require('../models/user.js');
 
 //Routers===========================================
 
+//GET (home page)
 router.get('/', async (req, res) => {
   try {
     res.render('applications/index.ejs');
@@ -14,8 +15,26 @@ router.get('/', async (req, res) => {
   }
 });
 
+//GET /new
 router.get('/new', async (req, res) => {
   res.render('applications/new.ejs');
+});
+
+//POST (form)
+router.post('/', async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);// Look up user from req.session
+
+    currentUser.applications.push(req.body);//Push req.body (new form data object) to apps array of current user
+
+    await currentUser.save();// Save changes to user
+
+    res.redirect(`/users/${currentUser._id}/applications`);// Redirect back to apps index view
+
+  } catch (error) {// If any errors, log and redirect back home
+    console.log(error);
+    res.redirect('/');
+  }
 });
 
 
